c5170c36dc83303ac6680cc0413a05aeb0adad1b
unknown <araves2003@gmail.com>
2022-12-30 23:09:36 -0800

    07

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,24 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    x=len(s1)
+    y=len(s2)
+    z=len(s3)
+    if x%2==0:
+        c=print("[s1]")
+    if y%2==0:
+        c=print("[s2]")
+    if z%2==0:
+        c=print("[s3]")
+    if x%2!=0:
+        c=print("[s1 toq]")
+    if y%2!=0:
+        c=print("[s2 toq]")
+    if z%2!=0:
+        c=print("[s3 toq]")
+    
+
+
+
+    return c 
+print(main("asd","asdf","asdfgh"))
